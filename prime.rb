78362ab0be17225509f509c3f3ca53a60d66c256
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-11 17:50:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -6,12 +6,12 @@ def prime?(int)
   else  #sieve
     num_array = (0..int).to_a #create an array of numbers 0 - int
     num[0] = num[1] = nil #set 0 and 1 to nil
-    
+
     num_array.each do |num| #iterate through num_array
       next if num.nil? #skips nil and continues the next iteration
       break if (num * num) > int
       num.step(int, num)  { |i| num_array[i] = nil }
-      binding.pry
+      #binding.pry
       #step through array in intervals of num until reaching int in question
       #replace each interval number with nil
     end
