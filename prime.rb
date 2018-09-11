6b55e2c9544c9e03a86404c1fb3c3399c3830971
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-11 17:47:55 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(int)
   if int < 0 || int == (0 || 1) #accounts for negative num, 0, and 1
     return false
   else  #sieve
-    num_array = (2..int).to_a #create an array of numbers 2 - int
+    num_array = (0..int).to_a #create an array of numbers 2 - int
 
     num_array.each do |num| #iterate through num_array
       next if num.nil? #skips nil values and continues the next iteration
