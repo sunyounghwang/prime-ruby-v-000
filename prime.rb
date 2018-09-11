b70a5a1c267b3224980a8a9a2d68d83aaabde577
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-11 17:50:25 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,15 @@
 require 'pry'
 
 def prime?(int)
-  if int < 0 || int == (0 || 1) #accounts for negative num, 0, and 1
+  if int < 0  #accounts for negatives
     return false
   else  #sieve
-    num_array = (0..int).to_a #create an array of numbers 2 - int
-
+    num_array = (0..int).to_a #create an array of numbers 0 - int
+    num[0] = num[1] = nil #set 0 and 1 to nil
+    
     num_array.each do |num| #iterate through num_array
-      next if num.nil? #skips nil values and continues the next iteration
-      #break if (num * num) > int
+      next if num.nil? #skips nil and continues the next iteration
+      break if (num * num) > int
       num.step(int, num)  { |i| num_array[i] = nil }
       binding.pry
       #step through array in intervals of num until reaching int in question
